fa02089bd85497434e1f5bcfbcf9ac3f85543404
João Henrique Freitas <joaohf2@gmail.com>
2023-08-21 10:52:02 -0300

    Criação de imagens e docker-compose dos serviços

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 
 DOCKER_VERSION=1.0.0
 DOCKER_NAME=erpfake
+
 ## build erpfake-client
 buildClient () {
     echo "Buildando o erpfake client";
-    cd erpfake-client && mvn clean install && cd ..
+    cd erpfake-client && mvn clean install 
+    docker build -t $DOCKER_NAME/client:$DOCKER_VERSION . && cd ..
 
-    if [[ "$?" -eq 0 ]]
+    if [[ "$?" -eq "1" ]]
     then
         echo "Ocorreu um erro ao tentar gerar a build do erp client"
         exit 1;
@@ -16,9 +18,11 @@ buildClient () {
 
 buildProduct() {
     echo "Buildando o erpfake product";
-    cd erpfake-client && mvn clean install && cd ..
+    cd erpfake-product && mvn clean install
+    echo "criando a imagem do servico de produto"
+    docker build -t $DOCKER_NAME/product:$DOCKER_VERSION . && cd ..
 
-    if [[ "$?" -eq 0 ]]
+    if [[ "$?" -eq "1" ]]
     then
         echo "Ocorreu um erro ao tentar gerar a build do erp produto"
         exit 1;
@@ -27,9 +31,12 @@ buildProduct() {
 
 buildEureka() {
     echo "Buildando o erpfake eureka register"
-    cd erpfake-eureka && mvn clean install && cd ..
-
-    if [[ "$?" -eq 0 ]]
+    cd erpfake-eureka && mvn clean install
+    
+    echo "criando a imagem do eureka server"
+    docker build -t $DOCKER_NAME/eureka:$DOCKER_VERSION . && cd ..
+    
+    if [[ "$?" -eq "1" ]]
     then
         echo "Ocorreu um erro ao tentar gerar a build do erp eureka register"
         exit 1;
@@ -38,15 +45,33 @@ buildEureka() {
 
 buildStock() {
     echo "Buildando o erpfake stock"
-    cd erpfake-stock && mvn clean install && cd ..
+    cd erpfake-stock && mvn clean install
+    echo "criando a imagem do servico de estoque"
+    docker build -t $DOCKER_NAME/stock:$DOCKER_VERSION . && cd ..
 
-    if [[ "$?" -eq 0 ]]
+    if [[ "$?" -eq "1" ]]
     then
         echo "Ocorreu um erro ao tentar gerar a build do erp stock";
         exit 1;
     fi
 }
 
+buildGateway() {
+    echo "Buildando o erpfake gateway"
+    cd erpfake-gateway && mvn clean install
+    echo "criando a imagem do servico de gateway"
+    docker build -t $DOCKER_NAME/gateway:$DOCKER_VERSION . && cd ..
+
+    if [[ "$?" -eq "1" ]]
+    then
+        echo "Ocorreu um erro ao tentar gerar a build do erp gateway";
+        exit 1;
+    fi
+}
+
+buildFrontend() {
+    echo "Ainda não implementado.... aguardando a vontade =)"   
+}
 
 case $1 in 
     build-all)
@@ -54,6 +79,7 @@ case $1 in
         buildProduct;
         buildEureka;
         buildStock;
+        buildGateway;
     ;;
     build-client)
         buildClient;
@@ -67,5 +93,8 @@ case $1 in
     ;;
     build-stock)
         buildStock;
+    ;;
+    build-gateway)
+        buildGateway;
     ;;
 esac
